588623aabef87e4d662517f7edeb523a8d139789
Ari Breitkreuz <ari.breitkreuz@pm.me>
2019-11-23 17:57:45 +0100

    Remove unused function in client

--- client/dont_touch.py
+++ client/dont_touch.py
@@ -1,26 +1,8 @@
 #! /usr/bin/python
 
 import pygit2
-import re
 import requests
 from time import sleep
-
-
-def get_function_name_from_line(path, lineno):
-    with open(path) as file:
-        # Check if we're in the main function
-        if not re.match('( |\t|def)', path[lineno]):
-            return '__main__'
-
-        for i in range(lineno, -1, -1):
-            # Split the 'def' off
-            split = path[i].split(maxsplit=1)
-            if split[0] == 'def':
-                # Split off the parameters
-                return split[1].split('(', maxsplit=1)[0]
-
-        raise RuntimeError("Indented but not in function")
-
 
 # Initialize repo
 repo = pygit2.Repository('.')
