134e92b25930f830c4f31518c343f3b43ebd15ef
Saman Miran <saman14m@gmail.com>
2019-11-23 21:03:04 +0100

    Highlighting fully functional

--- intellij-plugin/canttouchthis/src/main/java/components/HighlightComponent.java
+++ intellij-plugin/canttouchthis/src/main/java/components/HighlightComponent.java
@@ -2,26 +2,36 @@ package components;
 
 import com.intellij.codeInsight.daemon.impl.EditorTracker;
 import com.intellij.codeInsight.daemon.impl.EditorTrackerListener;
+import com.intellij.codeInsight.highlighting.HighlightManager;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ComponentManager;
 import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.EditorFactory;
+import com.intellij.openapi.editor.colors.EditorColors;
 import com.intellij.openapi.editor.colors.EditorColorsListener;
+import com.intellij.openapi.editor.colors.EditorColorsManager;
 import com.intellij.openapi.editor.colors.EditorColorsScheme;
 import com.intellij.openapi.editor.event.EditorEventMulticaster;
 import com.intellij.openapi.editor.ex.EditorEventMulticasterEx;
 import com.intellij.openapi.editor.ex.FocusChangeListener;
+import com.intellij.openapi.editor.markup.EffectType;
+import com.intellij.openapi.editor.markup.TextAttributes;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.ui.JBColor;
 import data.Config;
 import data.DataLoader;
+import data.model.Change;
 import data.model.File;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import resources.Colors;
 
 import java.awt.event.FocusEvent;
 import java.util.HashSet;
@@ -71,10 +81,36 @@ public class HighlightComponent implements ProjectComponent, FocusChangeListener
 
     @Override
     public void focusGained(@NotNull Editor editor) {
-        VirtualFile file = FileDocumentManager.getInstance().getFile(editor.getDocument());
-        String fileName = file.getName();
+        final Document document = editor.getDocument();
+        final VirtualFile file = FileDocumentManager.getInstance().getFile(document);
+        // TODO correct path formatting
+
+        String fileName = file.getPath() + file.getName();
         System.out.println("Trying to get file: " + fileName);
         File cttFileInformation = this.dataLoader.getFile(fileName);
+
+        if (cttFileInformation == null) return;
+
+        // Get required data keys
+        final Project project = editor.getProject();
+        final HighlightManager highlightManager = HighlightManager.getInstance(project);
+        final TextAttributes textAttributes = EditorColorsManager.getInstance().getGlobalScheme().getAttributes(EditorColors.LIVE_TEMPLATE_ATTRIBUTES);
+        textAttributes.setEffectType(EffectType.SEARCH_MATCH);
+
+        for (int i = 0; i < cttFileInformation.changes.length; ++i) {
+            Change c = cttFileInformation.changes[i];
+            TextAttributes newAttributes = textAttributes.clone();
+            newAttributes.setBackgroundColor(Colors.userColors[i % Colors.userColors.length]);
+
+            highlightManager.addRangeHighlight(
+                    editor,
+                    document.getLineStartOffset(c.lines[0].line),
+                    document.getLineStartOffset(c.lines[c.lines.length - 1].line + 1),
+                    newAttributes,
+                    false,
+                    null);
+        }
+
     }
 
     @Override
--- intellij-plugin/canttouchthis/src/main/java/resources/Colors.java
+++ intellij-plugin/canttouchthis/src/main/java/resources/Colors.java
@@ -5,7 +5,12 @@ import com.intellij.ui.JBColor;
 import java.awt.*;
 
 public class Colors {
-    Color[] userColors = {
-            JBColor.decode("#F0A0A0")
+    public final static Color[] userColors = {
+            JBColor.decode("#FFC0C0"),
+            JBColor.decode("#C0FFC0"),
+            JBColor.decode("#C0C0FF"),
+            JBColor.decode("#FFFFC0"),
+            JBColor.decode("#C0FFFF"),
+            JBColor.decode("#FFC0FF")
     };
 }
