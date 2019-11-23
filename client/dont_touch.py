ddafa37bc8cbe6f4625297f5b4836f8073258315
Ari Breitkreuz <ari.breitkreuz@pm.me>
2019-11-23 10:54:21 +0100

    Add getting function name from line number

--- client/dont_touch.py
+++ client/dont_touch.py
@@ -1,6 +1,24 @@
 #! /usr/bin/python
 
 from pygit2 import Repository
+import re
+
+
+def get_function_name_from_line(path, lineno):
+    with open(path) as file:
+        # Check if we're in the main function
+        if not re.match('( |\t|def)', path[lineno]):
+            return '__main__'
+
+        for i in range(lineno, -1, -1):
+            # Split the 'def' off
+            split = path[i].split(maxsplit=1)
+            if split[0] == 'def':
+                # Split off the parameters
+                return split[1].split('(', maxsplit=1)[0]
+
+        raise RuntimeError("Indented but not in function")
+
 
 # Stuff for diffs
 repo = Repository('.')
