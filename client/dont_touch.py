fe26148b65c2f70f285aa3f54cd7a843ea8b7d23
Tobias Juelg <tobias@juelg.net>
2019-11-23 20:03:42 +0100

    added parameter to change the working directory

--- client/dont_touch.py
+++ client/dont_touch.py
@@ -3,6 +3,10 @@
 import pygit2
 import requests
 from time import sleep
+import click
+import os
+
+SERVER = "52.236.180.203"
 
 # Initialize repo
 repo = pygit2.Repository('.')
@@ -15,31 +19,40 @@ remote = repo.branches['master'].upstream.remote_name
 url = repo.remotes[remote].url
 
 # Send to server
-server = 'http://localhost:8000/api/change/'
+server = 'http://{}:8080/api/change/'.format(SERVER)
 
-# Stuff for diffs
-while True:
-    diffs = []
-    for diff in repo.diff('master'):
-        path = diff.delta.old_file.path
-        lines = []
-        for hunk in diff.hunks:
-            for line in hunk.lines:
-                # Only check deleted lines
-                if line.new_lineno == -1:
-                    lines.append(line.old_lineno)
+@click.command()
+@click.option('--path', default=None, help='Path to the project root directory.')
+def main(path):
+    if path != None:
+        os.chdir(path)
+    
+    # Stuff for diffs
+    while True:
+        diffs = []
+        for diff in repo.diff('master'):
+            path = diff.delta.old_file.path
+            lines = []
+            for hunk in diff.hunks:
+                for line in hunk.lines:
+                    # Only check deleted lines
+                    if line.new_lineno == -1:
+                        lines.append(line.old_lineno)
 
-        # Ignore files which only have added lines
-        if len(lines) != 0:
-            diffs.append((path, lines))
+            # Ignore files which only have added lines
+            if len(lines) != 0:
+                diffs.append((path, lines))
 
-    for (path, changes) in diffs:
-        payload = {
-            'path': path,
-            'project': url,
-            'email': name,
-            'change': str(changes)
-        }
-        print(payload)
-        requests.post(server, payload)
-    sleep(10)
+        for (path, changes) in diffs:
+            payload = {
+                'path': path,
+                'project': url,
+                'email': name,
+                'change': str(changes)
+            }
+            print(payload)
+            requests.post(server, payload)
+        sleep(10)
+        
+if __name__ == "__main__":
+    main()
