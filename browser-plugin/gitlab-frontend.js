fab20d0727e8fe80d4f0f4d09af2bf87407fe34d
Conny Lin <cornelia.lin@tum.de>
2019-11-23 02:14:54 +0100

    Extend place_person_icons function to add several icons at once

--- browser-plugin/gitlab-frontend.js
+++ browser-plugin/gitlab-frontend.js
@@ -5,22 +5,23 @@ document.body.style.border = "5px solid red";
 var button = document.createElement("button");
 button.innerHTML = "Press me";
 
-function place_person_icon(filename) {
+function place_person_icons(filenames) {
     // Create icon
     var icon = document.createElement("img");
     icon.src = browser.runtime.getURL("icons/person.png");
     icon.style = "text-align:right; height:1em; width:auto";
-    // Find right table cell
-    var filename_text = Array.from(document.querySelectorAll("#tree-slider > tbody > tr > td.tree-item-file-name > a"))
-    .find(el => el.title === filename);
-    var filename_box = filename_text.parentElement;
+    // Find right table cells
+    var filename_texts = Array.from(document.querySelectorAll("#tree-slider > tbody > tr > td.tree-item-file-name > a"))
+    .filter(el => filenames.includes(el.title));
+    var filename_boxes = filename_texts.map(el => el.parentElement);
     // Arrange and insert elements
-    var divr = document.createElement("div");
-    divr.style = "float:right;width:10%;";
-    filename_box.append(divr);
-    divr.append(icon);
+    for (i=0; i<filename_boxes.length; i++) {
+        var divr = document.createElement("div");
+        divr.style = "float:right;width:10%;";
+        filename_boxes[i].append(divr);
+        divr.append(icon.cloneNode());
+    }
 }
 
 // Put person icon next to file object where text is ".idea"
-place_person_icon(".idea");
-//console.log(Array.from(document.querySelectorAll("#tree-slider > tbody > tr > td.tree-item-file-name > a")).find(el => el.title===".idea"));
+place_person_icons([".idea", ".gitignore"]);
