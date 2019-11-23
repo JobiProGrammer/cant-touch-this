188898ee0f77b5d764ee4fc72cbd9f97f9ba77f1
Conny Lin <cornelia.lin@tum.de>
2019-11-23 04:49:58 +0100

    Read number of users per file

--- browser-plugin/gitlab-frontend.js
+++ browser-plugin/gitlab-frontend.js
@@ -11,34 +11,50 @@ function get_curr_browser_dir() {
 
 // Filter out all filepaths that are not relevant for the current page
 // and format them to what is shown on gitlab
-function filter_to_scope (filenames) {
+function filter_to_scope (files) {
     var prefix = get_curr_browser_dir();
-    console.log("Current dir:", prefix);
-    return filenames.filter(n => n.startsWith(prefix)).map(s => s.substring(prefix.length).split("/")
-        .filter(s => s !== "")[0]);
+    var filtered_dict = {};
+    for (key of Object.keys(files)) {
+        if (key.startsWith(prefix)) {
+            cut_key = key.substring(prefix.length).split("/").filter(s => s !== "")[0];
+            if (cut_key in filtered_dict) {
+                filtered_dict[cut_key] += files[key];
+            } else {
+                filtered_dict[cut_key] = files[key];
+            }
+        }
+    }
+    return filtered_dict;
 }
 
 // Place person icons next to all files on the current page with the file names specified
-function place_person_icons(filenames) {
+function place_person_icons(files) {
+    files = filter_to_scope(files);
     // Create icon
     var icon = document.createElement("img");
     icon.src = browser.runtime.getURL("icons/person.png");
     icon.style = "text-align:right; height:1em; width:auto";
-    // Find right table cells
+    // Find respective table cells
     var filename_texts = Array.from(document.querySelectorAll("#tree-slider > tbody > tr > td.tree-item-file-name > a"))
-    .filter(el => filenames.includes(el.title));
+    .filter(el => Object.keys(files).includes(el.title));
     var filename_boxes = filename_texts.map(el => el.parentElement);
-    if (filename_texts.length != filename_boxes.length) {
-        console.log("LENGTH MISMATCH:", filename_boxes.length, filename_texts.length);
-    }
     // Arrange and insert elements
     for (i=0; i<filename_boxes.length; i++) {
+        // Number of people working
+        var num = document.createElement("span");
+        num.className = "badge badge-pill count issue-counter";
+        num.innerHTML = files[filename_texts[i].title];
+        // Person icon
         var divr = document.createElement("div");
-        divr.style = "float:right;width:10%;";
+        divr.style = "float:right; display: inline-block";
         filename_boxes[i].append(divr);
-        divr.append(icon.cloneNode());
+        divr.append(icon.cloneNode(), num);
     }
 }
 
 // MAIN
-place_person_icons(filter_to_scope([".gitignore", "browser-plugin/manifest.json", "browser-plugin/icons/person.png"]));
+place_person_icons({
+    ".gitignore": 1,
+    "browser-plugin/manifest.json": 2,
+    "browser-plugin/icons/person.png": 1
+});
